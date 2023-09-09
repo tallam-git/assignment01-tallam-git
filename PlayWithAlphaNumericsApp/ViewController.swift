a0b1c655dcecf14ac10f31cb45dfc4c6a1436ca6
Charan <tallamcharanteja99@gmail.com>
2023-09-08 20:02:12 -0500

    Initial Commit

---
+++ PlayWithAlphaNumericsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayWithAlphaNumericsApp
+//
+//  Created by Charan on 9/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
