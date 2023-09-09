66538950d73c249141da099d34c8dccffe8106d6
Charan <tallamcharanteja99@gmail.com>
2023-09-08 22:25:07 -0500

    Updated Header Label

--- PlayWithAlphaNumericsApp/ViewController.swift
+++ PlayWithAlphaNumericsApp/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var headerLBL: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.headerLBL.text = String(format: "%@\nPlay with AlphaNumerics™️","\u{0c38}\u{0c4d}\u{0c35}\u{0c3e}\u{0c17}\u{0c24}\u{0c02}")
     }
 
 
